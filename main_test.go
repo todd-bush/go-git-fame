9bb6a43001b9ee45675ece0d8e793d16e270210f
Todd Bush <todd.bush@lexisnexis.com>
2018-06-28 13:22:35 -0400

    making git_list_files test work

--- main_test.go
+++ main_test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func Test_git_list_files(t *testing.T) {
-	files := git_list_files(".", "develop")
+	files := git_list_files(".", "master")
 
 	if len(files) <= 0 {
 		t.Fatal("Expected files to be larger than zero")
+	} else {
+		fmt.Printf("files: %v\n", files)
 	}
 }
