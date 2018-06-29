cced14d74d244e69d95ec0303532adadd1f61bd8
Todd Bush <todd.bush@lexisnexis.com>
2018-06-28 21:22:13 -0400

    added 'is' for testing, modified git_list_files test

--- git_client/git_client_test.go
+++ git_client/git_client_test.go
@@ -3,15 +3,18 @@ package git
 import (
 	"fmt"
 	"testing"
+
+	"github.com/matryer/is"
 )
 
 func Test_git_list_files(t *testing.T) {
+
+	is := is.New(t)
+
 	files := git_list_files("master")
 
-	if len(files) <= 0 {
-		t.Fatal("Expecting files to be larger than zero")
-	} else {
-		fmt.Printf("files: %v\n", files)
-	}
+	is.True(len(files) > 0)
+
+	fmt.Printf("files: %v\n", files)
 
 }
